47ae528ad35ca81c3fca09d79757125e969e6eec
RoaaAbeery <148685929+RoaaAbeery@users.noreply.github.com>
2023-10-24 08:28:40 +0300

    Add files via upload

---
+++ assignment 1/src/Main.java
@@ -0,0 +1,127 @@
+import java.util.Scanner;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+//Question 1 :
+
+//       Scanner s1=new Scanner(System.in);
+//     System.out.println("pleas enter your weight(kg) :");
+//       double weight =s1.nextDouble();
+//       System.out.println("pleas enter your height(m) :");
+//       double height =s1.nextDouble();
+//    double bmi = weight / (height * height);
+//    System.out.println("BMI :" + bmi );
+
+//Question 2 :
+
+//        Scanner s2=new Scanner(System.in);
+//        System.out.println("enter the obtained Marks :" );
+//        double obtainedMarks = s2.nextDouble();
+//
+//        System.out.print("Enter total marks: ");
+//        double totalMarks = s2.nextDouble();
+//
+//        double percentage = (obtainedMarks / totalMarks) * 100;
+//        System.out.println("percentage = " + percentage + "%");
+
+//Question 3 :
+//
+//        Scanner s3 = new Scanner(System.in);
+//
+//        System.out.print("Enter the amount in USD: ");
+//        double amountInUSD = s3.nextDouble();
+//        System.out.print("Enter the exchange rate (USD to EUR): ");
+//        double exchangeRate = s3.nextDouble();
+//
+//        double amountInEUR = amountInUSD * exchangeRate;
+//
+//        System.out.println("Amount in EUR = " + amountInEUR);
+
+//Question 4 :
+
+//        Scanner s4=new Scanner(System.in);
+//        System.out.println(" Entre the String :");
+//        String inputString=s4.nextLine();
+//        StringBuilder reversedString = new StringBuilder(inputString).reverse();
+//        System.out.println("Length of the string: " + inputString.length());
+//        System.out.println("And Reversed string: " + reversedString);
+
+//Question5
+//
+//        Scanner s5 =new Scanner(System.in);
+//
+//        System.out.print("Enter a sentence: ");
+//        String sentence = s5.nextLine();
+//
+//        System.out.print("Enter the start index: ");
+//        int startIndex = s5.nextInt();
+//
+//        System.out.print("Enter the end index: ");
+//        int endIndex = s5.nextInt();
+//
+//        String extractedSubstring = sentence.substring(startIndex, endIndex);
+//
+//        System.out.println("Extracted Substring: \"" + extractedSubstring + "\"");
+
+
+//Question 6 :
+//
+//        Scanner s6 = new Scanner(System.in);
+//
+//        System.out.print("Enter a sentence: ");
+//        String sentence = s6.nextLine();
+//
+//        System.out.print("Enter a keyword: ");
+//        String keyword = s6.nextLine();
+//
+//        boolean isKeywordPresent = sentence.contains(keyword);
+//
+//        if (isKeywordPresent) {
+//            System.out.println("Keyword \"" + keyword + "\" is present in the sentence.");
+//        } else {
+//            System.out.println("Keyword \"" + keyword + "\" is not present in the sentence.");
+//        }
+
+
+// Question 7 :
+
+//
+//        Scanner s7 = new Scanner(System.in);
+//
+//        System.out.print("Enter a sentence: ");
+//        String sentence = s7.nextLine();
+//
+//        System.out.print("Enter the word to replace: ");
+//        String wordToReplace = s7.nextLine();
+//
+//        System.out.print("Enter the replacement word: ");
+//        String replacementWord = s7.nextLine();
+//
+//        String modifiedSentence = sentence.replaceAll(wordToReplace, replacementWord);
+//
+//        System.out.println("Modified Sentence: " + modifiedSentence);
+
+
+//Question 8 :
+
+//        Scanner s8 = new Scanner(System.in);
+//
+//        System.out.print("Enter the first string: ");
+//        String string1 = s8.nextLine();
+//
+//        System.out.print("Enter the second string: ");
+//        String string2 = s8.nextLine();
+//
+//        boolean areEqualIgnoreCase = string1.equalsIgnoreCase(string2);
+//
+//        if (areEqualIgnoreCase) {
+//            System.out.println("Strings are equal (ignoring case).");
+//        } else {
+//            System.out.println("Strings are not equal.");
+//        }
+
+    }
+}
